5303f2bc32bfa3306561a683c7568520c8db9440
ngthanhtra165 <tra16052002@gmail.com>
2024-06-14 16:53:36 +0700

    Initial Commit

---
+++ Threads Clone/Threads_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Threads_CloneApp.swift
+//  Threads Clone
+//
+//  Created by Macbook on 14/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Threads_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
